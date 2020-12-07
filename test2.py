4083542776104f74fcbf31b6aca9cc019d9a3105
hadi zarei <74762379+hadizarei87@users.noreply.github.com>
2020-12-07 14:28:22 +0330

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,6 @@
+countrey = {"name":"iran","time":+3.30,"capital":"tehran"}
+countrey["age avarage"] = 1,100
+countrey["name"] = "persian"
+print(countrey)
+print(type(countrey))
+print(countrey["time"])
---
+++ test3.py
@@ -0,0 +1,6 @@
+a = {"countery":{"name":["iran","germany","usa"],
+                "capital":["tehran","berlin","washington DC"],
+                "time":[12.30,10,22]}}
+
+
+print(a)
